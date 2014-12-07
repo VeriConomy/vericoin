c612b16044164ab94b08ba6aad32dcffcabc9c1e
onsightit <steve@onsightit.com>
2014-12-07 06:37:37 -0700

    removed debug values

--- src/version.h
+++ src/version.h
@@ -15,8 +15,7 @@ static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
                          +   40000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
-                         +       4 * CLIENT_VERSION_BUILD;
-// SDW DEBUG                         +       5 * CLIENT_VERSION_BUILD;
+                         +       5 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
