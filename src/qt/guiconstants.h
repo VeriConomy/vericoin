f93567ae9776ba6a64cd835b30156e2badd2085c
onsightit <onsightit@gmail.com>
2015-01-17 23:12:03 -0700

    changed statusbar margin

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -13,7 +13,7 @@ static const int MAX_PASSPHRASE_SIZE = 1024;
 
 /* BitcoinGUI -- Size of icons and left margin in status bar */
 static const int STATUSBAR_ICONSIZE = 16;
-static const int STATUSBAR_MARGIN = 10;
+static const int STATUSBAR_MARGIN = 8;
 
 /* Invalid field background style */
 #define STYLE_INVALID "background:#FF8080"
