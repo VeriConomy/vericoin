55fc8763752287c61a283867328bcaad078bc37b
onsightit <steve@onsightit.com>
2015-01-20 00:57:49 -0700

    flipped the chart gradient

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -155,8 +155,8 @@ OverviewPage::OverviewPage(QWidget *parent) :
     ui->listTransactions->setAttribute(Qt::WA_MacShowFocusRect, false);
     ui->listTransactions->setMouseTracking(true);
     ui->listTransactions->viewport()->setAttribute(Qt::WA_Hover, true);
-    ui->listTransactions->setStyleSheet("QListView { background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0, stop: 0 #fafbfe, stop: 1 #ECF3FA); color: " + STRING_VERIFONT + "; border-radius: 10px; border: 0; padding-right: 10px; padding-bottom: 5px; } \
-                                         QListView::hover { background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #fafbfe, stop: 1 #ECF3FA); }");
+    ui->listTransactions->setStyleSheet("QListView { background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #fafbfe, stop: 1 #ECF3FA); color: " + STRING_VERIFONT + "; border-radius: 10px; border: 0; padding-right: 10px; padding-bottom: 5px; } \
+                                         QListView::hover { background: qlineargradient(x1: 0, y1: 1, x2: 0, y2: 0, stop: 0 #fafbfe, stop: 1 #ECF3FA); }");
     ui->listTransactions->setFont(veriFont);
 
     connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
