0383de2ecb6b2a8741c09b3728b2e3e8a9889507
vericoin <vericoin@gmail.com>
2015-04-19 22:39:23 -0400

    adjusted stake parameters

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -15,8 +15,8 @@
 
 using namespace std;
 unsigned int nStakeSplitAge = 1 * 24 * 60 * 60;
-double nStakeSplitWeightFraction = 0.25;
-int64_t nStakeCombineThreshold = 500 * COIN;
+double nStakeSplitWeightFraction = 0.35;
+int64_t nStakeCombineThreshold = 1000 * COIN;
 
 //////////////////////////////////////////////////////////////////////////////
 //
