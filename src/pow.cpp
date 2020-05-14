4bbae00889f34b66ca8cf416a8a7643b3f95186f
Matthieu 'JP' DERASSE <github@derasse.fr>
2020-05-14 23:04:19 +0200

    Bignum ... will have to find a way to put it aside ...

--- src/pow.cpp
+++ src/pow.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2018 The Bitcoin Core developers
+// Copyright (c) 2009-2017 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -10,13 +10,14 @@
 #include <primitives/block.h>
 #include <uint256.h>
 #include <math.h>
+#include <bignum.h>
 #include <util/system.h>
 #include <timedata.h>
 #include <validation.h>
 
-
 #include <inttypes.h>
 
+/////////////////
 extern arith_uint256 proofOfWorkLimit;
 extern arith_uint256 proofOfWorkLimitTestNet;
 static const int64_t nTargetTimespan = 2 * 60 * 60;  // 2 hours
@@ -47,36 +48,35 @@ unsigned int GetNextTargetRequired(const CBlockIndex* pindexLast)
     unsigned int nTargetSpacing = calculateBlocktime(pindexPrev);
     int64_t nActualSpacing = pindexPrev->GetBlockTime() - pindexPrevPrev->GetBlockTime();
     int64_t targetTimespan;
-    if (pindexLast->nHeight+1 > 2394) {
+    if (pindexLast->nHeight+1 > 2394){
         targetTimespan = nTargetTimespan * 24;
-    } else {
-        targetTimespan = nTargetTimespan;
     }
+    else{targetTimespan = nTargetTimespan;}
     // ppcoin: target change every block
     // ppcoin: retarget with exponential moving toward target spacing
-    arith_uint256 bnNew;
+    CBigNum bnNew;
     bnNew.SetCompact(pindexPrev->nBits);
     int64_t nInterval = targetTimespan / nTargetSpacing;
     bnNew *= ((nInterval - 1) * nTargetSpacing + nActualSpacing + nActualSpacing);
     bnNew /= ((nInterval + 1) * nTargetSpacing);
-    if (bnNew > proofOfWorkLimit)
+    if (bnNew > CBigNum(proofOfWorkLimit))
     {
-        bnNew = proofOfWorkLimit;
+        bnNew = CBigNum(proofOfWorkLimit);
     }
     return bnNew.GetCompact();
 }
 
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
-    arith_uint256 target;
-    target.SetCompact(nBits);
+    CBigNum bnTarget;
+    bnTarget.SetCompact(nBits);
 
     // Check range
-    if (target <= 0 || target > proofOfWorkLimit)
+    if (bnTarget <= 0 || bnTarget > CBigNum(ArithToUint256(proofOfWorkLimit)))
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (UintToArith256(hash) > target)
+    if (UintToArith256(hash) > UintToArith256(bnTarget.getuint256()))
         return error("CheckProofOfWork() : hash doesn't match nBits");
 
     return true;
