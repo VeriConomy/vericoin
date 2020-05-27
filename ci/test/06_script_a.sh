4e00e42fd4d8cecb13240145f921b7837bcb0674
Matthieu 'JP' DERASSE <github@derasse.fr>
2020-05-27 18:51:29 +0200

    Disable test by default and let user enable it throught configure

--- ci/test/06_script_a.sh
+++ ci/test/06_script_a.sh
@@ -6,7 +6,7 @@
 
 export LC_ALL=C.UTF-8
 
-BITCOIN_CONFIG_ALL="--disable-dependency-tracking --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
+BITCOIN_CONFIG_ALL="--disable-dependency-tracking --enable-tests --enable-gui-tests --prefix=$DEPENDS_DIR/$HOST --bindir=$BASE_OUTDIR/bin --libdir=$BASE_OUTDIR/lib"
 if [ -z "$NO_DEPENDS" ]; then
   DOCKER_EXEC ccache --max-size=$CCACHE_SIZE
 fi
