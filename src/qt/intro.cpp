8c3f5157d6d102a13cfd72f8cd9693dca3c9a379
Douglas Pike <dpike@vericoin.info>
2020-06-23 07:11:12 -0400

    QT: fix typo on intro

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -127,7 +127,7 @@ Intro::Intro(QWidget *parent, uint64_t blockchain_size, uint64_t chain_state_siz
     ui->lblExplanation1->setText(ui->lblExplanation1->text()
         .arg(PACKAGE_NAME)
         .arg(m_blockchain_size)
-        .arg(2017)
+        .arg(2016)
         .arg(tr("Verium"))
     );
     ui->lblExplanation2->setText(ui->lblExplanation2->text().arg(PACKAGE_NAME));
