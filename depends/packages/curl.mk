0d201903b012b83ad40f5dda12361dd4ba97ed5d
Matthieu 'JP' DERASSE <github@derasse.fr>
2020-07-06 08:48:50 +0200

    Improve build process

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -3,10 +3,10 @@ $(package)_version=7.62.0
 $(package)_download_path=https://curl.haxx.se/download/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=55ccd5b5209f8cc53d4250e2a9fd87e6f67dd323ae8bd7d06b072cfcbb7836cb
-$(package)_dependencies=openssl
+$(package)_dependencies=openssl zlib
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared
+$(package)_config_opts=--disable-shared --with-ca-fallback
 $(package)_config_opts_mingw32=--enable-sspi --without-ssl --with-winssl --with-schannel
 $(package)_config_opts_darwin=--enable-sspi --without-ssl --with-darwinssl --with-secure-transport
 $(package)_cflags=-DCURL_STATICLIB
--- depends/packages/minizip.mk
+++ depends/packages/minizip.mk
@@ -6,6 +6,9 @@ $(package)_sha256_hash=78d87c463dd6bd446292fe0d3ee72878355b3b7fa52add5722671dfe9
 $(package)_dependencies=zlib
 
 define $(package)_set_vars
+$(package)_config_opts=--enable-cxx --disable-shared --enable-static
+$(package)_config_opts_linux=--with-pic
+
 ifeq ($(host),x86_64-apple-darwin16)
 $(package)_cc=$(BASEDIR)/$(host)/native/bin/clang
 $(package)_cxx=$(BASEDIR)/$(host)/native/bin/clang++
